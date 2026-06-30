f9e5e85f40e32c69246c0de06a93afd3711ada4b
WilGulf <wilgot.ulfstedt@outlook.com>
2026-06-30 23:59:53 +0200

    Changed kernel to be a higher half kernel

--- kernel/loader.s
+++ kernel/loader.s
@@ -15,11 +15,52 @@ align 4
     dd CHECKSUM
 
 section .bss
-align 4
-kernel_stack:
-    resb KERNEL_STACK_SIZE
+align 16
+stack_bottom:
+    resb 16384 * 8
+stack_top:
+
+section .boot
+
+;align 4
+;kernel_stack:
+;    resb KERNEL_STACK_SIZE
 
 section .text
 loader:
-    mov esp, kernel_stack + KERNEL_STACK_SIZE
+    mov eax, (initial_page_dir - 0xC0000000)
+    mov cr3, eax
+
+    mov ecx, cr4
+    or ecx, 0x00000010
+    mov cr4, ecx
+
+    mov ecx, cr0
+    or ecx, 0x80000000
+    mov cr0, ecx
+
+    jmp higher_half
+
+section .text
+higher_half:
+    mov esp, stack_top
+    push ebx
+    xor ebp, ebp
+
     call kmain
+halt:
+    hlt
+    jmp halt
+
+section .data
+align 4096
+global initial_page_dir
+initial_page_dir:
+    dd 10000011B
+    times 768-1 dd 0
+
+    dd (0 << 22) | 10000011B
+    dd (1 << 22) | 10000011B
+    dd (2 << 22) | 10000011B
+    dd (3 << 22) | 10000011B
+    times 256-4 dd 0
