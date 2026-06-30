ea27135d7e3d60922816ccff32714926c7eb154b
WilGulf <wilgot.ulfstedt@outlook.com>
2026-06-30 14:43:32 +0200

    Changed idt names

--- kernel/idt/idt_asm.s
+++ kernel/idt/idt_asm.s
@@ -4,89 +4,89 @@ load_idt:
     lidt [eax]
     ret
 
-%macro ISR_NOERRORCODE 1
-    global isr%1
-    isr%1:
+%macro no_error_code_interrupt_handler 1
+    global interrupt_handler_%1
+    interrupt_handler_%1:
         cli
-        push long 0
-        push long %1
-        jmp isr_common_stub
+        push dword 0
+        push dword %1
+        jmp common_interrupt_handler
 %endmacro
 
-%macro ISR_ERRORCODE 1
-    global isr%1
-    isr%1:
+%macro error_code_interrupt_handler 1
+    global interrupt_handler_%1
+    interrupt_handler_%1:
         cli
-        push long %1
-        jmp isr_common_stub
+        push dword %1
+        jmp common_interrupt_handler
 %endmacro
 
-%macro IRQ 2
+%macro irq_interrupt 2
     global irq%1
     irq%1:
         cli
-        push long 0
-        push long %2
-        jmp irq_common_stub
+        push dword 0
+        push dword %2
+        jmp irq_interrupt_handler
 %endmacro
 
-ISR_NOERRORCODE 0
-ISR_NOERRORCODE 1
-ISR_NOERRORCODE 2
-ISR_NOERRORCODE 3
-ISR_NOERRORCODE 4
-ISR_NOERRORCODE 5
-ISR_NOERRORCODE 6
-ISR_NOERRORCODE 7
+no_error_code_interrupt_handler 0
+no_error_code_interrupt_handler 1
+no_error_code_interrupt_handler 2
+no_error_code_interrupt_handler 3
+no_error_code_interrupt_handler 4
+no_error_code_interrupt_handler 5
+no_error_code_interrupt_handler 6
+no_error_code_interrupt_handler 7
 
-ISR_ERRORCODE 8
-ISR_ERRORCODE 9
-ISR_ERRORCODE 10
-ISR_ERRORCODE 11
-ISR_ERRORCODE 12
-ISR_ERRORCODE 13
-ISR_ERRORCODE 14
+error_code_interrupt_handler 8
+error_code_interrupt_handler 9
+error_code_interrupt_handler 10
+error_code_interrupt_handler 11
+error_code_interrupt_handler 12
+error_code_interrupt_handler 13
+error_code_interrupt_handler 14
 
-ISR_NOERRORCODE 15
-ISR_NOERRORCODE 16
-ISR_NOERRORCODE 17
-ISR_NOERRORCODE 18
-ISR_NOERRORCODE 19
-ISR_NOERRORCODE 20
-ISR_NOERRORCODE 21
-ISR_NOERRORCODE 22
-ISR_NOERRORCODE 23
-ISR_NOERRORCODE 24
-ISR_NOERRORCODE 25
-ISR_NOERRORCODE 26
-ISR_NOERRORCODE 27
-ISR_NOERRORCODE 28
-ISR_NOERRORCODE 29
-ISR_NOERRORCODE 30
-ISR_NOERRORCODE 31
+no_error_code_interrupt_handler 15
+no_error_code_interrupt_handler 16
+no_error_code_interrupt_handler 17
+no_error_code_interrupt_handler 18
+no_error_code_interrupt_handler 19
+no_error_code_interrupt_handler 20
+no_error_code_interrupt_handler 21
+no_error_code_interrupt_handler 22
+no_error_code_interrupt_handler 23
+no_error_code_interrupt_handler 24
+no_error_code_interrupt_handler 25
+no_error_code_interrupt_handler 26
+no_error_code_interrupt_handler 27
+no_error_code_interrupt_handler 28
+no_error_code_interrupt_handler 29
+no_error_code_interrupt_handler 30
+no_error_code_interrupt_handler 31
 
-ISR_NOERRORCODE 128
-ISR_NOERRORCODE 177
+no_error_code_interrupt_handler 128
+no_error_code_interrupt_handler 177
 
-IRQ 0, 32
-IRQ   1,    33
-IRQ   2,    34
-IRQ   3,    35
-IRQ   4,    36
-IRQ   5,    37
-IRQ   6,    38
-IRQ   7,    39
-IRQ   8,    40
-IRQ   9,    41
-IRQ  10,    42
-IRQ  11,    43
-IRQ  12,    44
-IRQ  13,    45
-IRQ  14,    46
-IRQ  15,    47
+irq_interrupt 0, 32
+irq_interrupt   1,    33
+irq_interrupt   2,    34
+irq_interrupt   3,    35
+irq_interrupt   4,    36
+irq_interrupt   5,    37
+irq_interrupt   6,    38
+irq_interrupt   7,    39
+irq_interrupt   8,    40
+irq_interrupt   9,    41
+irq_interrupt  10,    42
+irq_interrupt  11,    43
+irq_interrupt  12,    44
+irq_interrupt  13,    45
+irq_interrupt  14,    46
+irq_interrupt  15,    47
 
-extern isr_handler
-isr_common_stub:
+extern interrupt_handler
+common_interrupt_handler:
     pusha
     mov eax, ds
     push eax
@@ -100,8 +100,7 @@ isr_common_stub:
     mov gs, ax
 
     push esp
-
-    call isr_handler
+    call interrupt_handler
 
     add esp, 8
     pop ebx
@@ -115,7 +114,7 @@ isr_common_stub:
     iret
 
 extern irq_handler
-irq_common_stub:
+irq_interrupt_handler:
     pusha
     mov eax,ds
     push eax
