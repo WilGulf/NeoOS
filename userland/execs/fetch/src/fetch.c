ca7076f8ec9e042183cc22fa93e461392cc690d5
WilGulf <wilgot.ulfstedt@outlook.com>
2026-08-22 19:22:34 +0200

    fixed fetch logo

--- userland/execs/fetch/src/fetch.c
+++ userland/execs/fetch/src/fetch.c
@@ -15,8 +15,8 @@ int main(int argc, char **argv) {
     printf("MAl  M              M  M  M         Kernel: %s-%d-%d-%d.%s\n", info.version.name, info.version.version_major, info.version.version_minor, info.version.version_patch, info.version.extension);
     printf("M A  M  dMMb  dMMb  M  M  M         Machine: i386\n");
     printf("M ll M  M  M  M  M  M  M  TMMb      \n");
-    printf("M  A M  ?MMM  M  M  M  M     M      \n");
-    printf("M  lLM     M  M  M  M  M     M      \n");
+    printf("M  A M  MMMP  M  M  M  M     M      \n");
+    printf("M  lLM  M     M  M  M  M     M      \n");
     printf("M.  VM  ?MMP  ?MMP  ?MMP  ?MMP      \n");
 
     exit();
