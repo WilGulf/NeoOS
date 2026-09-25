d275a2ccb7a5c6d182c8c1c877a9cb2301e98696
WilGulf <245902168+WilGulf@users.noreply.github.com>
2026-09-25 09:11:32 +0200

    Fixed licensing

--- kernel/drivers/fs/file.h
+++ kernel/drivers/fs/file.h
@@ -1,3 +1,23 @@
+/*
+ *
+ * Copyright (C) 2026 Daniel McCarthy <daniel@dragonzap.com>
+  * This file is drerived from the PeachOS Kernel (github.com/nibblebits/PeachOS).
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ * 
+ * See NOTICE for the full list of GPL-derived files in this project.
+ */
+
 #ifndef FILE_H
 #define FILE_H
 
